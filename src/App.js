6fcc3c2d8b68ba4dde8ea2293e72869797fe4cb4
chenxi <chenxi.li2@uqconnect.edu.au>
2024-09-14 15:24:50 +1000

    current-weather widget

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import './App.css';
+import styles from './App.less';
 import Search from './components/search/search';
+import CurrentWeather from './components/current-weather/current-weather';
 
 function App() {
-
   const handleOnSearchChange = (searchData) => {
     console.log(123)
   }
 
   return (
-    <div className="container">
+    <div className={styles.container}>
       <Search onSearchChange={handleOnSearchChange}/>
+      <CurrentWeather />
     </div>
   );
 }
