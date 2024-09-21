f6dd9c997f431899d6adf393dfaea65876b133a2
chenxi <chenxi.li2@uqconnect.edu.au>
2024-09-21 21:12:47 +1000

    add sshagent for ec2

--- script.groovy
+++ script.groovy
@@ -24,11 +24,21 @@ def dockerPushImage() {
 
 def deploy() {
     echo "dploy the app to ec2 server..."
-    def dockerCmd = "docker run -d -p 80:80 sunshinerxx/react-weather-app:1.1"
+
+    def dockerCmd = "docker run -d -p 80:80 --name react-weather-app sunshinerxx/react-weather-app:1.1"
     def ec2Instance = "ec2-user@3.107.6.214"
-    // sshagent(['ec2-docker-key']) {
-    //     sh "ssh -o StrictHostKeyChecking=no ${ec2Instance} ${dockerCmd}"
-    // }
+    sshagent(['ec2-docker-key']) {
+        withCredentials([
+            usernamePassword(
+                credentialsId: "docker-hub",
+                usernameVariable: "USER",
+                passwordVariable: "PASS"
+            )
+        ]) {
+            sh "ssh -o StrictHostKeyChecking=no ${ec2Instance} echo $PASS | docker login -u $USER --password-stdin"
+            sh "ssh -o StrictHostKeyChecking=no ${ec2Instance} ${dockerCmd}"
+        }
+    }
 }
 
 return this
