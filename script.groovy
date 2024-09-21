972e35b11d74103d52a45b2466b66c47676497f9
chenxi <chenxi.li2@uqconnect.edu.au>
2024-09-21 21:59:51 +1000

    docker compose

--- script.groovy
+++ script.groovy
@@ -25,7 +25,7 @@ def dockerPushImage() {
 def deploy() {
     echo "dploy the app to ec2 server..."
 
-    def dockerRunCmd = "docker run -d -p 80:80 --name react-weather-app sunshinerxx/react-weather-app:1.1"
+    def dockerCmd = "cd /home/ec2-user && docker-compose -f docker-compose.yaml up"
     def ec2Instance = "ec2-user@3.107.6.214"
     sshagent(['ec2-docker-key']) {
         withCredentials([
