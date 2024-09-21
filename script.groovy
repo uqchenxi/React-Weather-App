19fb4522de55ef75f88d9b7c250c864567c35470
chenxi <chenxi.li2@uqconnect.edu.au>
2024-09-21 22:57:41 +1000

    change path

--- script.groovy
+++ script.groovy
@@ -25,7 +25,7 @@ def dockerPushImage() {
 def deploy() {
     echo 'deploy the app to ec2 server...'
 
-    def dockerCmd = 'cd /home/ec2-user/ && docker-compose -f docker-compose.yaml up'
+    def dockerCmd = 'docker-compose -f /home/ec2-user/docker-compose.yaml up'
     def ec2Instance = "ec2-user@3.107.6.214"
     sshagent(['ec2-docker-key']) {
         withCredentials([
